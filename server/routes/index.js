95172fa367d5a544f173efe9caa7a64f57adf4c2
jalen <cedric.bridges3@gmail.com>
2025-01-18 14:38:29 -0600

    feat: updated bcrypt

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const uniquid = require('../utils/uniquid');
